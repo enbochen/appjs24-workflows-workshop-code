f0e9cbcfbd6e5cd95fa5b79ccab14dae254d6b32
Enbo Chen <enbo.chen@kesko.fi>
2024-05-22 09:49:09 +0200

    add share functionality

--- app/works/[id]/index.tsx
+++ app/works/[id]/index.tsx
@@ -14,6 +14,7 @@ import { useFavStatusMutation } from "@/data/hooks/useFavStatusMutation";
 import colors from "@/constants/colors";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { LoadingShade } from "@/components/LoadingShade";
+import * as Sharing from "expo-sharing";
 
 export default function DisplayWork() {
   const dimensions = useWindowDimensions();
@@ -61,7 +62,7 @@ export default function DisplayWork() {
             <Text className="flex-1 font-semibold text-3xl px-4 py-2 bg-shade-2">
               {work?.title}
             </Text>
-            <View className="justify-center px-4 flex-row items-center">
+            <View className="justify-center px-4 flex-row items-center gap-3">
               <Pressable
                 className="active:opacity-50"
                 disabled={favQuery.isLoading || favMutation.isPending}
@@ -75,6 +76,9 @@ export default function DisplayWork() {
                   size={28}
                 />
               </Pressable>
+              <Link push href={`/works/${id}/share`}>
+                <Icon name="share-alt" color={colors.tint} size={28} />
+              </Link>
             </View>
           </View>
           <View className="px-4 gap-y-2 py-2">
@@ -86,7 +90,9 @@ export default function DisplayWork() {
           </View>
           {work?.description && (
             <>
-              <Text className="text-xl font-semibold px-4 py-2 bg-shade-2">Description</Text>
+              <Text className="text-xl font-semibold px-4 py-2 bg-shade-2">
+                Description
+              </Text>
               <View className="px-4 gap-y-2 py-2">
                 <Text className="text-l">{stripTags(work.description)}</Text>
               </View>
